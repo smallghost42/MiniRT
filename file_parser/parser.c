8d81ea81129ca711956e1aa76f90b0e1dee1804b
smallghost <fenoherymamindrainy@gmail.com>
2024-12-05 11:24:41 +0300

    ggg

--- file_parser/parser.c
+++ file_parser/parser.c
@@ -2,6 +2,12 @@
 #include "get_next_line.h"
 #include <string.h>
 
+void ft_perror(char *message, int err_stat)
+{
+	errno = err_stat;
+	perror(message);
+}
+
 int check_file_extention(char *filename)
 {
 	char *str;
@@ -14,15 +20,30 @@ int check_file_extention(char *filename)
 	return (1);
 }
 
+int check_file_content(int file)
+{
+	char *str;
+
+	str = get_next_line(file, 1);
+	if (str == NULL)
+	{
+		ft_perror("error , empty line ", 22);
+		return (1);
+	}
+	check_first_word_and_count_A_C_L();
+	return (0);
+}
+
 int file_parser(char *filename)
 {
 	int file;
-	char *str;
 
+	int stat;
+
+	stat = 0;
 	if (check_file_extention(filename) == 0)
 	{
-		errno = 22;
-		perror("error , file extention ");
+		ft_perror("error , file extention ", 22);
 		return (1);
 	}
 	file = open(filename, O_RDONLY);
@@ -31,13 +52,7 @@ int file_parser(char *filename)
 		perror("error ");
 		return (1);
 	}
-	str = get_next_line(file, 1);
-	if (str == NULL)
-	{
-		errno = 22;
-		perror("error , empty line ");
+	if (check_file_content(file) == 1)
 		return (1);
-	}
-	close(file);
 	return (0);
 }
