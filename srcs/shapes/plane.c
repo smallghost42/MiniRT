d90943272baeed143fc12b94bf8574bc412e94a7
endy-rkt <fenoherymamindrainy@gmail.com>
2024-12-27 16:09:30 -0600

    :sparkles: specular + diffuse light for plane

--- srcs/shapes/plane.c
+++ srcs/shapes/plane.c
@@ -6,7 +6,7 @@
 /*   By: trazanad <trazanad@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 08:29:59 by trazanad          #+#    #+#             */
-/*   Updated: 2024/12/27 15:53:09 by trazanad         ###   ########.fr       */
+/*   Updated: 2024/12/27 16:04:51 by trazanad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,55 @@ static int get_diffuse_light_color(t_ray ray, float distance, t_vec3 normal_vec)
     return (color);
 }
 
+
+static int get_specular_light_color(t_ray ray, float distance, t_vec3 normal_vec) 
+{
+    t_vec3 point_to_light_vec;
+    t_vec3 halfway_vec;
+    float spec_brightness;
+    float tmp;
+    int color;
+    int trgb[4];
+    float shininess = 64.0;
+
+    t_vec3 light_pos = vec3_create(0, 100, -200);
+    point_to_light_vec = get_point_to_light_vector(light_pos, ray, distance);
+
+    halfway_vec = vec3_normalize(vec3_add(point_to_light_vec, ray.direction));
+
+    tmp = vec3_get_dot_product(normal_vec, halfway_vec);
+    spec_brightness = powf(fmax(tmp, 0.1), shininess);
+
+    //suppose light reflexision is white nigga
+    int surface_trgb[4] = {1, 255, 255, 255};
+    trgb[0] = 1;
+    trgb[1] = fmin(roundf(surface_trgb[1] * spec_brightness), 255);
+    trgb[2] = fmin(roundf(surface_trgb[2] * spec_brightness), 255);
+    trgb[3] = fmin(roundf(surface_trgb[3] * spec_brightness), 255);
+
+    color = get_color_from_trgb(trgb[0], trgb[1], trgb[2], trgb[3]);
+    return (color);
+}
+
+static int add_colors(int diffuse_color, int specular_color) 
+{
+    int trgb[4];
+    int *diffuse_trgb;
+    int *specular_trgb;
+    
+	diffuse_trgb = get_trgb_from_color(diffuse_color);
+	specular_trgb = get_trgb_from_color(specular_color);
+
+    trgb[0] = 1;
+    trgb[1] = fmin(diffuse_trgb[1] * 0.8 + specular_trgb[1] * 0.5, 255);
+    trgb[2] = fmin(diffuse_trgb[2] * 0.8 + specular_trgb[2] * 0.5, 255);
+    trgb[3] = fmin(diffuse_trgb[3] * 0.8 + specular_trgb[3] * 0.5, 255); 
+    free(diffuse_trgb);
+    free(specular_trgb);
+    return get_color_from_trgb(trgb[0], trgb[1], trgb[2], trgb[3]);
+}
+
+
 int render_plane(t_scene *scene, t_vec3 camera_pos, t_vec3 plane_point, t_vec3 plane_normal)
 {
 	int     coord[2]; //x, y
@@ -70,7 +119,11 @@ int render_plane(t_scene *scene, t_vec3 camera_pos, t_vec3 plane_point, t_vec3 p
                 if (distance > current_distance)
                     distance = current_distance;
 				// int color = get_color_from_trgb(1, 0, 255, 255);
-				int color = get_diffuse_light_color(ray, current_distance, plane_normal);
+				int	color = add_colors(get_diffuse_light_color(ray, current_distance, plane_normal), 
+                           get_specular_light_color(ray, current_distance, plane_normal));
+
+				// int color = get_diffuse_light_color(ray, current_distance, plane_normal);
+				// int color = get_specular_light_color(ray, current_distance, plane_normal);
 
                 my_mlx_pixel_put(scene, coord[0], coord[1], color);
             }
